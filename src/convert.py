7a87ee4155f998d1d9db2e3495ea412165920c42
peixed351 <peixed351@gmail.com>
2024-02-08 13:31:31 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,22 @@
-import supervisely as sly
+import gzip
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from urllib.parse import unquote, urlparse
 
+import cv2
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import (
+    file_exists,
+    get_file_name,
+    mkdir,
+    remove_dir,
+    silent_remove,
+)
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +39,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +67,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +76,116 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    temp_path = "/home/alex/DATASETS/TODO/STARE/temp"
+    images_path = "/home/alex/DATASETS/TODO/STARE/all-images"
+    ah_masks_path = "/home/alex/DATASETS/TODO/STARE/labels-ah"
+    vk_masks_path = "/home/alex/DATASETS/TODO/STARE/labels-vk"
+    results_4_masks_path = "/home/alex/DATASETS/TODO/STARE/results-4"
+    diagnoses_path = "/home/alex/DATASETS/TODO/STARE/diagnoses.txt"
+    batch_size = 30
+    ds_name = "ds"
+    arch_path_to_ext = {
+        ah_masks_path: ".ah.ppm.gz",
+        vk_masks_path: ".vk.ppm.gz",
+        results_4_masks_path: "-vessels4.ppm.gz",
+    }
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    images_ext = ".png"
 
-    # ... some code here ...
+    mkdir(temp_path)
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    def create_ann(image_path):
+        labels = []
+        tags = []
 
-    # return project
+        img_height = 605  # mask_np.shape[0]
+        img_wight = 700  # mask_np.shape[1]
 
+        diagnos_value = im_name_to_diagnos.get(get_file_name(image_path))
+        if diagnos_value is not None:
+            if len(diagnos_value) > 0:
+                diagnos = sly.Tag(diagnoses_meta, value=diagnos_value)
+                tags.append(diagnos)
 
+        for arch_path, arch_ext in arch_path_to_ext.items():
+            curr_arch_path = os.path.join(arch_path, get_file_name(image_path) + arch_ext)
+            if file_exists(curr_arch_path):
+                labeller_meta = arch_path_to_meta[arch_path]
+                labeller = sly.Tag(labeller_meta)
+                curr_image_path = curr_arch_path[: -1 * len(arch_ext)] + images_ext
+                with gzip.open(curr_arch_path, "rb") as f_in:
+                    with open(curr_image_path, "wb") as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+                        mask_np = cv2.imread(curr_image_path)[:, :, 0]
+                        obj_mask = mask_np == 255
+                        curr_bitmap = sly.Bitmap(obj_mask)
+                        curr_label = sly.Label(curr_bitmap, obj_class, tags=[labeller])
+                        labels.append(curr_label)
+                silent_remove(curr_image_path)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+    obj_class = sly.ObjClass("vessels", sly.Bitmap)
+
+    ah_meta = sly.TagMeta("adam hoover", sly.TagValueType.NONE)
+    vk_meta = sly.TagMeta("valentina kouznetsova", sly.TagValueType.NONE)
+    results_meta = sly.TagMeta("filter probing algorithm", sly.TagValueType.NONE)
+
+    diagnoses_meta = sly.TagMeta("diagnos", sly.TagValueType.ANY_STRING)
+
+    arch_path_to_meta = {
+        ah_masks_path: ah_meta,
+        vk_masks_path: vk_meta,
+        results_4_masks_path: results_meta,
+    }
+
+    im_name_to_diagnos = {}
+
+    with open(diagnoses_path) as f:
+        content = f.read().split("\n")
+        for row in content:
+            data = row.split("\t")
+            im_name_to_diagnos[data[0]] = data[-1]
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[obj_class], tag_metas=[ah_meta, vk_meta, results_meta, diagnoses_meta]
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_names = os.listdir(images_path)
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+    for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+        new_im_names_batch = []
+        images_pathes_batch = []
+        for im_name in img_names_batch:
+            im_path = os.path.join(images_path, im_name)
+            new_im_name = get_file_name(im_name) + images_ext
+            new_im_names_batch.append(new_im_name)
+            new_im_path = os.path.join(temp_path, new_im_name)
+            images_pathes_batch.append(new_im_path)
+
+            curr_image_np = cv2.imread(im_path)
+            cv2.imwrite(new_im_path, curr_image_np)
+
+        img_infos = api.image.upload_paths(dataset.id, new_im_names_batch, images_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns_batch)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    remove_dir(temp_path)
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,42 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Stare"
+PROJECT_NAME_FULL: str = "STARE: Structured Analysis of the Retina"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Medical()]
+CATEGORY: Category = Category.Medical()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.SemanticSegmentation()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2000
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://cecas.clemson.edu/~ahoover/stare/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 13323368
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/stare"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "Images": "https://cecas.clemson.edu/~ahoover/stare/images/all-images.zip",
+    "Adam Hoover labelled data": "https://cecas.clemson.edu/~ahoover/stare/probing/labels-ah.tar",
+    "Valentina Kouznetsova labelled data": "https://cecas.clemson.edu/~ahoover/stare/probing/labels-vk.tar",
+    "Spatial filter probing algorithm labelled data": "https://cecas.clemson.edu/~ahoover/stare/probing/results-4.tar",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,19 +56,26 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = {
+    "Research Paper1": "https://ieeexplore.ieee.org/abstract/document/845178",
+    "Research Paper2": "https://ieeexplore.ieee.org/abstract/document/1216219",
+    "Academic Torrents": "https://academictorrents.com/details/e4554cd63400dc13b74477efe98032c10757c269",
+}
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Michael Goldbaum"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["mgoldbaum@ucsd.edu"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "University of California, USA"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://ucsd.edu/"
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "labellers": ["adam hoover", "valentina kouznetsova", "filter probing algorithm"],
+    "__POSTTEXT__": "Additionally, every image contains information about patient ***diagnos***",
+}
 TAGS: Optional[List[str]] = None
 
 
