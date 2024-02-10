2f4b4b09487a200f0861ce9c47f8fab962199913
peixed351 <peixed351@gmail.com>
2024-02-10 17:33:45 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "STARE"
 PROJECT_NAME_FULL: str = "STARE: Structured Analysis of the Retina Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
